ae7f7dd596f9d2c6139ac7ba6e75f0ebadabf2ed
marcelosrg <marcelohrs2005@gmail.com>
2025-05-03 16:57:19 -0300

    feat: adicionando repositorio para operações crud no banco de dados

---
+++ backend/src/main/java/com/verbum/backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.verbum.backend.repository;
+
+import com.verbum.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
