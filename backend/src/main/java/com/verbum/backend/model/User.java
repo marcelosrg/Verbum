47dd89d29bfba081c3fd8f632e9364e36e9b559d
marcelosrg <marcelohrs2005@gmail.com>
2025-05-03 17:01:01 -0300

    feat: removendo temporariamente os campos updated_at e created_at

--- backend/src/main/java/com/verbum/backend/model/User.java
+++ backend/src/main/java/com/verbum/backend/model/User.java
@@ -2,12 +2,16 @@ package com.verbum.backend.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity(name = "USUARIO")
 @Data
+/*@EntityListeners(AuditingEntityListener.class)*/
 public class User {
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name="IDUSUARIO", unique = true, nullable = false)
@@ -29,9 +33,11 @@ public class User {
     @Column(name = "PROFILEURL", nullable = true)
     private String image;
 
+    /*@CreatedDate
     @Column(name = "CREATED_AT", nullable = false)
     private LocalDateTime created_at;
 
+    @LastModifiedDate
     @Column(name = "UPDATED_AT", nullable = true)
-    private LocalDateTime updated_at;
+    private LocalDateTime updated_at;*/
 }
