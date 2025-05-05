1ecb8d24a789206a916a2acf0fb48e67071ff8eb
marcelosrg <marcelohrs2005@gmail.com>
2025-05-05 20:21:21 -0300

    feat: adicionando serviço para listar todos os usuarios

--- backend/src/main/java/com/verbum/backend/services/UserServices.java
+++ backend/src/main/java/com/verbum/backend/services/UserServices.java
@@ -1,9 +1,13 @@
 package com.verbum.backend.services;
 
 import com.verbum.backend.dto.RequestUserDto;
+import com.verbum.backend.dto.ResponseUserDto;
 import com.verbum.backend.mapper.UserMapper;
 import com.verbum.backend.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServices {
@@ -19,5 +23,10 @@ public class UserServices {
 
     public void createUser(RequestUserDto userDto){
         userRepository.save(userMapper.RequestUserDtoToUser(userDto));
+    }
+
+    public List<ResponseUserDto> getAllUsers(){
+        var users = userRepository.findAll();
+        return this.userMapper.UserToListUserDto(users).stream().collect(Collectors.toList());
     }
 }
