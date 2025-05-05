623866bd6ea4345c7ca98621f42a375f0d156010
marcelosrg <marcelohrs2005@gmail.com>
2025-05-05 20:19:53 -0300

    feat: adicionando mapeamento de entidade para dto

---
+++ backend/src/main/java/com/verbum/backend/dto/ResponseUserDto.java
@@ -0,0 +1,4 @@
+package com.verbum.backend.dto;
+
+public class ResponseUserDto {
+}
--- backend/src/main/java/com/verbum/backend/mapper/UserMapper.java
+++ backend/src/main/java/com/verbum/backend/mapper/UserMapper.java
@@ -1,11 +1,15 @@
 package com.verbum.backend.mapper;
 
 import com.verbum.backend.dto.RequestUserDto;
+import com.verbum.backend.dto.ResponseUserDto;
 import com.verbum.backend.model.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     User RequestUserDtoToUser(RequestUserDto requestUserDto);
+    List<ResponseUserDto> UserToListUserDto(List<User> user);
 }
