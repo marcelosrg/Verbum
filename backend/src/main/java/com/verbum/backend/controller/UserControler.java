b51db3b3bebdebd86f07a9b51a68fc69134e4c97
marcelosrg <marcelohrs2005@gmail.com>
2025-05-05 21:04:06 -0300

    feat: adicionando funcionalidade de busca por nome de usuario

--- backend/src/main/java/com/verbum/backend/controller/UserControler.java
+++ backend/src/main/java/com/verbum/backend/controller/UserControler.java
@@ -4,11 +4,13 @@ import com.verbum.backend.dto.RequestUserDto;
 import com.verbum.backend.dto.ResponseUserDto;
 
 import com.verbum.backend.services.UserServices;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/users")
@@ -22,6 +24,11 @@ public class UserControler {
     @GetMapping("/getAll")
     public ResponseEntity<List<ResponseUserDto>> getAll(){
         var users = userServices.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+    @GetMapping("/{name}")
+    public ResponseEntity<List<ResponseUserDto>> getByName(@PathVariable String name){
+        var users = userServices.getByName(name);
         return ResponseEntity.ok(users);
     }
 
